c4ab5caa05172af63c715aff625d100329015488
vagrant <vagrant@debian-7.8.0-amd64>
2016-06-01 22:14:03 +0000

    Updated for whatever I have on my Vagrant machine

---
+++ link-vagrant-dotfiles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PROJ_NAME="vagrant_dotfiles"
+
+echo "Starting script to link vagrant dotfiles"
+cd ~
+ln -s $PROJ_NAME/bash_aliases .bash_aliases
+rm .irbrc
+ln -s $PROJ_NAME/irbrc .irbrc
+ln -s $PROJ_NAME/pryrc .pryrc
+echo "Finished linking the dotfiles"
